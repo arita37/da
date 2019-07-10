a92a637f56af63d2395e249ee6beed52e812ea8d
arita37 <noelkev0@gmail.com>
2019-07-10 09:10:55 +0900

    updated

--- da/util_feature.py
+++ da/util_feature.py
@@ -32,8 +32,42 @@ print("os.getcwd", os.getcwd())
 
 ####################################################################################################
 ####################################################################################################
-def pd_stat_statistics(df) :
-    pass
+def pd_col_findtype(df) :
+  """
+  :param df:
+  :return:
+  """
+  n = len(df) + 0.0
+  colcat , colnum, coldate, colother = [], [], [], []
+  for x in df.columns :
+      nunique = len( df[x].unique())
+      ntype = str(df[x].dtype)
+      r =  nunique /n
+      print(r, nunique, ntype )
+
+      if r > 0.90 :
+          colother.append(x)
+
+
+      elif nunique < 3 :
+          colcat.append(x)
+
+      elif ntype == "o" and nunique < 100 :
+          colcat.append(x)
+
+      elif nunique > 50 and ( "float" in ntype or  "int" in ntype ):
+          colnum.append(x)
+
+      else :
+          colother.append(x)
+
+  return colcat , colnum, coldate, colother
+
+
+
+# pd_col_findtype(df)
+
+
 
 
 
--- da/ztest.py
+++ da/ztest.py
@@ -20,6 +20,46 @@ df[ "Fare_bin" ]
 
 
 
+def pd_col_findtype(df) :
+  """
+  :param df:
+  :return:
+  """
+  n = len(df) + 0.0
+  colcat , colnum, coldate, colother = [], [], [], []
+  for x in df.columns :
+      nunique = len( df[x].unique())
+      ntype = str(df[x].dtype)
+      r =  nunique /n
+      print(r, nunique, ntype )
+
+
+      if r > 0.90 :
+          colother.append(x)
+
+
+      elif nunique < 3 :
+          colcat.append(x)
+
+      elif ntype == "o"  :
+          colcat.append(x)
+
+      elif nunique > 50 and ( "float" in ntype or  "int" in ntype ):
+          colnum.append(x)
+
+      else :
+          colother.append(x)
+
+  return colcat , colnum, coldate, colother
+
+
+
+pd_col_findtype(df) 
+
+
+
+
+
 
 
 
